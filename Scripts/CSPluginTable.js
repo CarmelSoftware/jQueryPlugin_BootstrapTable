e7aac6f7a33be31d109dfd8bad617cc47da03ded
Carmel Shvartzman <carmel.aspnet@gmail.com>
2016-01-05 19:15:33 +0200

    Update CSPluginTable.js

--- Scripts/CSPluginTable.js
+++ Scripts/CSPluginTable.js
@@ -1,1 +1,12 @@
-/////////////////////////
+jQuery(function ($) {
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+    /// jQuery Plugin to display a  Panel Table with Bootstrap style
+    ///       useful for displaying blog comments, comment threads, articles lists, Tweets, or any list 
+    ///       featuring a left aligned image and textual content.
+    ///
+    /// 
+    /// Uou can send MVC data inside the opts : { data: MVCdata }
+    /// Else, you can Ajax load the data, specifying the URL : { url : "Scripts/TableItems.js" }
+    /// opts = { data : JSobject, url : "MyJSONData.js" }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
